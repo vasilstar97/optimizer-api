65b8c9084e50e9f17a9d523bf217fdc3074489d4
vasilstar <vasilstar97@gmail.com>
2025-02-09 17:01:09 +0300

    feat: zoning based on lu_igu library

--- app/api/routers/zoning/zoning_service.py
+++ app/api/routers/zoning/zoning_service.py
@@ -3,9 +3,18 @@ import shapely
 import geopandas as gpd
 import random
 from loguru import logger
-from blocksnet.preprocessing.land_use_optimizer import LandUseOptimizer, LandUse
 from ...utils import const
 from .zoning_models import Profile
+from lu_igi.preprocessing.graph import generate_adjacency_graph
+from lu_igi.optimization.optimizer import Optimizer
+from lu_igi.optimization.problem import FitnessType
+from lu_igi.models.land_use import LandUse
+
+FITNESS_TYPES = [
+    FitnessType.ADJACENCY_PENALTY, 
+    FitnessType.PROBABILITY, 
+    FitnessType.SHARE_MSE
+]
 
 DEFAULT_CRS = 4326
 
@@ -15,18 +24,27 @@ def _get_profile_lu_shares(profile : Profile) -> dict[LandUse, float]: #FIXME re
     probabilities = [value / total for value in random_values]
     return dict(zip(LandUse, probabilities))
 
+def _generate_adjacency_graph(blocks_gdf : gpd.GeoDataFrame):
+    blocks_gdf['land_use'] = None
+    return generate_adjacency_graph(blocks_gdf)
+
 def generate_zoning(profile : Profile, blocks_gdf : gpd.GeoDataFrame, max_iter : int, rate : float):
 
     local_crs = blocks_gdf.estimate_utm_crs()
     blocks_gdf = blocks_gdf.to_crs(local_crs)
 
-    lu_shares = _get_profile_lu_shares(profile)
-    
-    logger.info('Initializing land use optimizer')
-    luo = LandUseOptimizer(blocks_gdf)
+    graph = _generate_adjacency_graph(blocks_gdf)
+    optimizer = Optimizer(graph)
 
-    logger.info('Solving the optimization problem')
-    best_X, best_value, _, _ = luo.run(lu_shares, max_iter=max_iter, rate=rate)
-    logger.success(f'Problem solved, fit value equals {round(best_value,3)}')
+    target_lu_shares = _get_profile_lu_shares(profile)
+    blocks_ids = list(blocks_gdf.index)
 
-    return luo.to_gdf(best_X)
+    results = optimizer.run(blocks_ids, target_lu_shares, FITNESS_TYPES, n_eval=max_iter, verbose=False)
+
+    best_solutions = optimizer.best_solutions(results, FITNESS_TYPES)
+    best_solution = best_solutions[FitnessType.SHARE_MSE]['X']
+
+    blocks_gdf = optimizer.to_gdf(best_solution,blocks_ids,blocks_gdf.crs)
+    blocks_gdf['land_use'] = blocks_gdf['assigned_land_use'].apply(lambda lu : lu.value)
+
+    return blocks_gdf[['geometry', 'land_use']]
